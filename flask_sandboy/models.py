c545eb579586346ff74d0a3607edf217969574c8
hsum <hsum@users.noreply.github.com>
2015-01-09 14:09:14 +0800

    columns with defaults are no longer required
    If a column in the model has a defined default value, then the request shouldn't have to have it.  Let SQLAlchemy handle the defaulting.

--- flask_sandboy/models.py
+++ flask_sandboy/models.py
@@ -41,7 +41,7 @@ def verify_fields(function):
             raise BadRequestException("No data received from request")
         for required in instance.__model__.__table__.columns:
             if required.name in (
-                    instance.__model__.__table__.primary_key.columns):
+                    instance.__model__.__table__.primary_key.columns) or required.default:
                 continue
             if required.name not in data:
                 raise ForbiddenException('{} required'.format(required))
